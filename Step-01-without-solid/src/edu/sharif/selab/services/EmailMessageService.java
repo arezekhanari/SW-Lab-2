4ffd233b53ed21975acbc7b04c05e8930e95499e
amkamir82 <amk_amir82@yahoo.com>
2024-10-31 20:30:19 +0330

    add TelegramMessageService to MessageServices and implement them in other other services

--- Step-01-without-solid/src/edu/sharif/selab/services/EmailMessageService.java
+++ Step-01-without-solid/src/edu/sharif/selab/services/EmailMessageService.java
@@ -2,10 +2,11 @@ package edu.sharif.selab.services;
 
 import edu.sharif.selab.models.EmailMessage;
 import edu.sharif.selab.models.SmsMessage;
+import edu.sharif.selab.models.TelegramMessage;
 
 import java.util.regex.Pattern;
 
-public class EmailMessageService implements MessageService{
+public class EmailMessageService implements MessageService {
     @Override
     public void sendSmsMessage(SmsMessage smsMessage) {
         //Empty Body
@@ -13,11 +14,16 @@ public class EmailMessageService implements MessageService{
 
     @Override
     public void sendEmailMessage(EmailMessage emailMessage) {
-        if(validateEmailAddress(emailMessage.getSourceEmailAddress()) && validateEmailAddress(emailMessage.getTargetEmailAddress())){
+        if (validateEmailAddress(emailMessage.getSourceEmailAddress()) && validateEmailAddress(emailMessage.getTargetEmailAddress())) {
             System.out.println("Sending a SMS from " + emailMessage.getSourceEmailAddress() + " to " + emailMessage.getTargetEmailAddress() + " with content : " + emailMessage.getContent());
-        }else{
+        } else {
             throw new IllegalArgumentException("Email Address is Not Correct!");
         }
+    }
+
+    @Override
+    public void sendTelegramMessage(TelegramMessage telegramMessage) {
+
     }
 
     public boolean validateEmailAddress(String email) {
--- Step-01-without-solid/src/edu/sharif/selab/services/SmsMessageService.java
+++ Step-01-without-solid/src/edu/sharif/selab/services/SmsMessageService.java
@@ -2,13 +2,14 @@ package edu.sharif.selab.services;
 
 import edu.sharif.selab.models.EmailMessage;
 import edu.sharif.selab.models.SmsMessage;
+import edu.sharif.selab.models.TelegramMessage;
 
-public class SmsMessageService implements MessageService{
+public class SmsMessageService implements MessageService {
     @Override
     public void sendSmsMessage(SmsMessage smsMessage) {
-        if(validatePhoneNumber(smsMessage.getSourcePhoneNumber()) && validatePhoneNumber(smsMessage.getTargetPhoneNumber())){
+        if (validatePhoneNumber(smsMessage.getSourcePhoneNumber()) && validatePhoneNumber(smsMessage.getTargetPhoneNumber())) {
             System.out.println("Sending a SMS from " + smsMessage.getSourcePhoneNumber() + " to " + smsMessage.getTargetPhoneNumber() + " with content : " + smsMessage.getContent());
-        }else{
+        } else {
             throw new IllegalArgumentException("Phone Number is Not Correct!");
         }
     }
@@ -16,6 +17,11 @@ public class SmsMessageService implements MessageService{
     @Override
     public void sendEmailMessage(EmailMessage emailMessage) {
         //Empty Body!
+    }
+
+    @Override
+    public void sendTelegramMessage(TelegramMessage telegramMessage) {
+
     }
 
     private boolean validatePhoneNumber(String phoneNumber) {
